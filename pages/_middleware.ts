621875e632aa9b9c0095cdcbaf102b1b53ba5e1d
Samuel_Roellin <samfromaway@gmail.com>
2022-03-31 08:18:08 +0200

    test

---
+++ pages/_middleware.ts
@@ -0,0 +1,14 @@
+import { NextRequest, NextResponse } from 'next/server';
+
+const delay = (ms: number) => new Promise((res) => setTimeout(res, ms));
+
+export default async function middleware(req: NextRequest) {
+  const url = req.nextUrl.clone();
+
+  await delay(5000);
+  const res = await fetch('https://62453f937701ec8f724f2890.mockapi.io/menu');
+  const menu = await res.json();
+  console.log(menu);
+
+  return NextResponse.rewrite(url);
+}
