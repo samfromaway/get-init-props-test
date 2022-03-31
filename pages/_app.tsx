750892c5095e2a8201f1d8ac033305522223a2f5
Samuel_Roellin <samfromaway@gmail.com>
2022-03-31 08:02:07 +0200

    test

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -15,14 +15,17 @@ function MyApp({ Component, pageProps }: AppProps) {
   );
 }
 
+const delay = (ms: number) => new Promise((res) => setTimeout(res, ms));
+
 MyApp.getInitialProps = async ({ Component, ctx }: AppContext) => {
   let pageProps = {};
   if (Component.getInitialProps) {
     pageProps = await Component.getInitialProps(ctx);
   }
+
+  await delay(5000);
   const res = await fetch('https://62453f937701ec8f724f2890.mockapi.io/menu');
   const menu = await res.json();
-  console.log(menu);
 
   return {
     pageProps: {
