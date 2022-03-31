9c421eeec151ab63072a5c8a389dea6bfe0abd4f
Samuel_Roellin <samfromaway@gmail.com>
2022-03-31 08:19:04 +0200

    test

--- pages/_middleware.ts
+++ pages/_middleware.ts
@@ -5,7 +5,7 @@ const delay = (ms: number) => new Promise((res) => setTimeout(res, ms));
 export default async function middleware(req: NextRequest) {
   const url = req.nextUrl.clone();
 
-  await delay(5000);
+  await delay(3000);
   const res = await fetch('https://62453f937701ec8f724f2890.mockapi.io/menu');
   const menu = await res.json();
   console.log(menu);
