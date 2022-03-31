f80dd1859b3b5e3361f7af02339ead4e77fff6f3
Samuel_Roellin <samfromaway@gmail.com>
2022-03-31 07:54:56 +0200

    init

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,35 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppContext, AppProps } from 'next/app';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  console.log(pageProps.menu);
+
+  return (
+    <>
+      <div style={{ display: 'flex' }}>
+        {pageProps.menu.map((e: any) => (
+          <h3 key={e.id}>{e.item}</h3>
+        ))}
+      </div>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+MyApp.getInitialProps = async ({ Component, ctx }: AppContext) => {
+  let pageProps = {};
+  if (Component.getInitialProps) {
+    pageProps = await Component.getInitialProps(ctx);
+  }
+  const res = await fetch('https://62453f937701ec8f724f2890.mockapi.io/menu');
+  const menu = await res.json();
+  console.log(menu);
+
+  return {
+    pageProps: {
+      ...pageProps,
+      menu,
+    },
+  };
+};
+
+export default MyApp;
